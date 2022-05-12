2d319b7afc016ed71e9dda06c178c364049a960f
ak <arurnkumar@gmail.com>
2022-05-12 18:14:56 +0530

    removed unusd

--- spark/src/main/java/spark/InputFromDiskEx.java
+++ spark/src/main/java/spark/InputFromDiskEx.java
@@ -19,12 +19,6 @@ public class InputFromDiskEx {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) 
 	{
-		List<String> inputData = new ArrayList<>();
-		inputData.add("WARN: Tuesday 4 September 0405");
-		inputData.add("ERROR: Tuesday 4 September 0408");
-		inputData.add("FATAL: Wednesday 5 September 1632");
-		inputData.add("ERROR: Friday 7 September 1854");
-		inputData.add("WARN: Saturday 8 September 1942");
 		
 		Logger.getLogger("org.apache").setLevel(Level.WARN);
 		
